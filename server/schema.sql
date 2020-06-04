57e75ff2ed3384254a1a6ca138473cd20dc3999b
Chelsea Schmidt <cschmidt331@gmail.com>
2020-06-04 10:01:51 -0700

    Designed schema for database and started writing SQL query

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,19 @@ CREATE DATABASE chat;
 
 USE chat;
 
+/*CREATE USERS TABLE*/
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  username VARCHAR(255) NOT NULL,
+  objectId INT NOT NULL AUTO_INCREMENT,
+  text VARCHAR(255) NOT NULL,
+  createdAt DATE,
+  roomname VARCHAR(255) NOT NULL,
+  updatedAt DATE,
+  FOREIGN KEY (username) REFERENCES users(username)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 
 /* Create other tables and define schemas for them here! */
